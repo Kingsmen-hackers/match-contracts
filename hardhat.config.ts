1cdafa26b8501cdbc8339d5454698d827bcad3b4
Akachukwu Chukwuemerie David <63329343+Imdavyking@users.noreply.github.com>
2024-08-16 11:03:37 +0100

    fixed compilation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
-  typechain: {
-    outDir: "types",
-    target: "ethers-v5",
-  },
 };
 
 export default config;
