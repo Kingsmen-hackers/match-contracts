b90be8b3646d0646b1ab9f5ff6459d9e2ffce101
imdavyking <yungdynamic53@gmail.com>
2024-11-26 22:43:00 +0100

    updated

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200, // Reduce this if size is still an issue (e.g., 50 or 100).
+      },
+    },
+  },
 };
 
 export default config;
